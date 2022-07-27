e68444c4a4a2e36f9639866188034d9d5a9e77a0
ChebetTrizah <trizachebet@kabarak.ac.ke>
2022-07-27 10:12:37 +0300

    final

--- src/main/java/com/example/pesaFix/service/EmailService.java
+++ src/main/java/com/example/pesaFix/service/EmailService.java
@@ -21,8 +21,3 @@
 //        SimpleMailMessage message = new SimpleMailMessage();
 //        message.setTo(to);
 //        message.setSubject(subject);
-//        message.setText(body);
-//        mailSender.send(message);
-//    }
-//
-//}
